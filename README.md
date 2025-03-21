90b50342e3d08afd138e522e5c46f40ce81c805d
Éamonn Ó Nualláin <eamonn.o.nuallain@gmail.com>
2025-03-21 10:57:34 +0000

    intial commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# backstage-stub
+# [Backstage](https://backstage.io)
 
-## Project stub
+This is your newly scaffolded Backstage App, Good Luck!
+
+To start the app, run:
+
+```sh
+yarn install
+yarn dev
+```
---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "# backstage-stub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:eonuallain/backstage-stub.git
+git push -u origin main
+
